e812a1990bd13a585a4a22cb90adace01b3e3c20
maheshbasnet089 <basnetmanish089@gmail.com>
2024-08-30 13:50:40 +0545

    upcoming

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views.auth_view import renderRegisterForm,renderLoginForm
-from .views.home_view import renderHomepage,renderCreateBlogPage
+from .views.home_view import blog_list,create_blog,blog_detail,delete_blog,update_blog
+
 
 urlpatterns  = [
-    path('',renderHomepage),
+    path('', blog_list, name='blog_list'),
     path('register',renderRegisterForm), 
     path('login',renderLoginForm), 
-    path('create',renderCreateBlogPage)
+    path('<int:blog_id>/', blog_detail, name='blog_detail'),
+    path('create/', create_blog, name='create_blog'),
+    path('<int:blog_id>/edit/', update_blog, name='update_blog'),
+    path('<int:blog_id>/delete/', delete_blog, name='delete_blog'),
+
 ]
--- myproject/blog/views/home_view.py
+++ myproject/blog/views/home_view.py
@@ -1,10 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from ..models import Blog
 
 # Create your views here.
 
-def renderHomepage(request):
-    return render(request,'home/index.html')
 
 
-def renderCreateBlogPage(request):
-    return render(request,'home/create_blog.html')
+# Create a new blog post
+def create_blog(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        subtitle = request.POST.get('subtitle')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+
+        blog = Blog(title=title, subtitle=subtitle, description=description, image=image)
+        blog.save()
+
+        return redirect('blog_list')  # Redirect to the list of blog posts
+
+    return render(request, 'home/create_blog.html')
+
+# List all blog posts
+def blog_list(request):
+    blogs = Blog.objects.all().order_by('-created_at')
+    return render(request, 'home/blog_list.html', {'blogs': blogs})
+
+# View a single blog post
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, 'home/blog_detail.html', {'blog': blog})
+
+# Update an existing blog post
+def update_blog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    if request.method == 'POST':
+        blog.title = request.POST.get('title')
+        blog.subtitle = request.POST.get('subtitle')
+        blog.description = request.POST.get('description')
+        if request.FILES.get('image'):
+            blog.image = request.FILES.get('image')
+        blog.save()
+
+        return redirect('blog_detail', blog_id=blog.id)
+
+    return render(request, 'home/update_blog.html', {'blog': blog})
+
+# Delete a blog post
+def delete_blog(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('blog_list')
+
+    return render(request, 'home/delete_blog.html', {'blog': blog})
